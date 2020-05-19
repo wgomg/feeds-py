06ef6479e320c3a52342a8f680ec24da6d3ea0e2
wgomg <w.gomez.g@protonmail.ch>
2020-05-18 21:04:25 -0400

    Initial Commit

---
+++ __init__.py
@@ -0,0 +1,35 @@
+from feedgen.feed import FeedGenerator
+import flask
+
+from scrapper import get_emol_news
+
+app = flask.Flask(__name__)
+app.config['debug'] = True
+
+
+@app.route('/emol', methods=['GET'])
+def emol():
+    news = get_emol_news()
+
+    fg = FeedGenerator()
+
+    fg.title(news['title'])
+    fg.description(news['description'])
+    fg.link(href=news['link'])
+
+    for article in news['articles']:
+        fe = fg.add_entry()
+        fe.title(article['title'])
+        fe.link(href=article['link'])
+        fe.description(article['description'])
+        fe.guid(article['link'], permalink=True)
+        fe.author(name=article['author'])
+        fe.pubDate(article['pubDate'])
+
+    res = flask.make_response(fg.rss_str())
+    res.headers.set('Content-Type', 'application/xml')
+    return res
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ scrapper.py
@@ -0,0 +1,39 @@
+import requests
+import json
+from bs4 import BeautifulSoup
+import datetime
+import dateutil.parser
+
+
+def get_emol_news():
+    r = requests.get(
+        'https://cache-elastic-pandora.ecn.cl/emol/noticia/_search?q=publicada:true+AND+seccion:nacional&sort=fechaPublicacion:desc&size=60&from=0')
+
+    news = {
+        'title': 'Emol Nacional',
+        'description': 'Últimas noticias nacionales',
+        'link': 'http://www.emol.com',
+        'articles': []
+    }
+
+    if r.status_code == 200:
+        json_data = r.json()
+        news_data = json_data['hits']['hits']
+
+        articles = []
+
+        for article_data in news_data:
+            article = {
+                'title': article_data['_source']['titulo'],
+                'link': article_data['_source']['permalink'],
+                'description': article_data['_source']['texto'],
+                'author': article_data['_source']['fuente'],
+                'pubDate': dateutil.parser.parse(
+                    article_data['_source']['fechaPublicacion']+'Z')
+            }
+
+            articles.append(article)
+
+        news['articles'] = articles
+
+    return news
