0354e132f9cf1b2f11c6fa787b87086860439327
wgomg <w.gomez.g@protonmail.ch>
2020-05-22 17:38:28 -0400

    Añade feed para latercera.cl

--- __init__.py
+++ __init__.py
@@ -3,6 +3,7 @@ import flask
 
 from scrapper import get_emol_news
 from scrapper import get_biobio_news
+from scrapper import get_lt3_news
 
 app = flask.Flask(__name__)
 app.config['debug'] = True
@@ -35,6 +36,30 @@ def emol():
 @app.route('/biobio', methods=['GET'])
 def biobio():
     news = get_biobio_news()
+
+    fg = FeedGenerator()
+
+    fg.title(news['title'])
+    fg.description(news['description'])
+    fg.link(href=news['link'])
+
+    for article in news['articles']:
+        fe = fg.add_entry()
+        fe.title(article['title'])
+        fe.link(href=article['link'])
+        fe.description(article['description'])
+        fe.guid(article['link'], permalink=True)
+        fe.author(name=article['author'])
+        fe.pubDate(article['pubDate'])
+
+    res = flask.make_response(fg.rss_str())
+    res.headers.set('Content-Type', 'application/xml')
+    return res
+
+
+@app.route('/lt3', methods=['GET'])
+def lt3():
+    news = get_lt3_news()
 
     fg = FeedGenerator()
 
--- scrapper.py
+++ scrapper.py
@@ -1,6 +1,7 @@
 import requests
 import json
 from bs4 import BeautifulSoup
+import datetime
 import dateutil.parser
 
 
@@ -73,4 +74,41 @@ def get_biobio_news():
     return news
 
 
-get_biobio_news()
+def get_lt3_news():
+    news_requests = []
+
+    for page in range(1, 2):
+        news_requests.append(requests.get(
+            'https://www.latercera.com/lo-ultimo/page/' + str(page)))
+
+    news = {
+        'title': 'La Tercera - Lo Ultimo',
+        'description': 'Ultimas noticias',
+        'link': 'https://www.latercera.com/',
+        'articles': []
+    }
+
+    articles = []
+    for r in news_requests:
+        if r.status_code == 200:
+            soup = BeautifulSoup(r.content, "html.parser")
+
+            for article_data in soup.find_all("article", {"class": "card | border-bottom float"}):
+                pubDateArray = article_data.find("div", {"class": "time"}).small.contents[1].string.strip(
+                ).lower().replace('hace ', '').split(' ')
+                minutes = int(pubDateArray[0]) if pubDateArray[1] == 'minutes' else int(
+                    pubDateArray[0])*60
+
+                article = {
+                    'title': article_data.find("h3").find("a").contents[2].string.strip(),
+                    'link': "https://www.latercera.com" + article_data.find("h3").a.get("href"),
+                    'description': article_data.find("div", {"class": "deck | isText"}).p.text,
+                    'author': article_data.find("div", {"class": "name"}).find("small").text,
+                    'pubDate': datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(minutes=minutes)
+                }
+
+                articles.append(article)
+
+    news['articles'] = articles
+
+    return news
